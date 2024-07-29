f15a2ae87bcac642198f657a250ba95fc80c41d2
ChongYuXiang <s10257151@connect.np.edu.sg>
2024-07-30 00:44:15 +0800

    AI Nav
    AI navigation imported
    Nav mesh added to terrain
    Basic enemy chasing script created

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent zombie;
+    public Transform target;
+    public float min_range;
+    public float max_range;
+
+    private void Update()
+    {
+        if (zombie != null || target != null)
+        {
+            float dist = Vector3.Distance(target.transform.position, transform.position);
+
+            if (dist <= max_range)
+            {
+                zombie.stoppingDistance = min_range;
+                zombie.SetDestination(target.position);
+            }
+            if (dist <= min_range)
+            {
+                transform.LookAt(target.position);
+                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            }
+            
+        }
+    }
+}
